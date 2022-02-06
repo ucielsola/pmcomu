8629835f39f7b8afe5741cfe350eb5f5f5a3af2d
Uciel Sola <solauciel@gmail.com>
2022-02-06 16:47:11 -0300

    added content

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,183 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import EmaiIcon from '$lib/components/emaiIcon.svelte';
+	import WhatsappIcon from '$lib/components/whatsappIcon.svelte';
+</script>
+
+<header>
+	<img src="" alt="PM Logo" />
+	<div class="text-wrapper">
+		<h1>Comunicaciones</h1>
+		<h2>La comunicación constituye y a la vez fluye como la vida.</h2>
+	</div>
+</header>
+
+<main>
+	<section>
+		<article>
+			<h3>Comunicación, Innovación, Estrategia</h3>
+			<p>
+				La comunicación estratégica, integral y orgánica es un intangible que influye en los
+				productos, servicios y crear valor. En el actual contexto volátil e incierto, lleno de
+				complejidad y ambigüedad creciente, el liderazgo, la comunicación y el uso de la tecnología
+				amplían las perspectivas y los aprendizajes de nuevas capacidades, para liderar entornos
+				flexibles y empoderados . Creamos y facilitamos para que su mensaje llegue correctamente a
+				los públicos que deben recibirlo.
+			</p>
+		</article>
+		<img src="/assets/images/img-1.webp" alt="" />
+	</section>
+
+	<section>
+		<article>
+			<h3>QUIÉNES SOMOS</h3>
+			<h4>
+				PM Comunicaciones es una empresa de asesoramiento en diseño y ejecución de estrategias de
+				comunicación y liderazgo.
+			</h4>
+			<p>
+				Nos dirigimos a marcas, personas en puestos de liderazgo, empresas, organizaciones
+				gubernamentales y no gubernamentales.
+			</p>
+			<p>
+				Somos aliados en el de desafío de fortalecer su identidad, haciendo visible la gestión,
+				cultura, filosofía y responsabilidad social, en definitiva potenciando el valor de la propia
+				trayectoria.
+			</p>
+		</article>
+		<img src="/assets/images/img-2.webp" alt="" />
+	</section>
+
+	<section>
+		<article>
+			<h3>COMUNICACIÓN DIGITAL</h3>
+			<h4>
+				Logramos el posicionamiento a través de las plataformas virtuales, proporcionando una
+				comunicación clara y asertiva.
+			</h4>
+			<p>
+				Las Nuevas Tecnologías de la Información cambiaron radicalmente las formas, modos, tiempos y
+				canales de comunicación de las organizaciones y empresas. La comunicación digital es un gran
+				reto, pero también una oportunidad. Se convirtieron en una herramienta eficaz de gestión,
+				innovación, buen servicios y productividad.
+			</p>
+			<p>
+				Creamos, organizamos y gestionamos los contenidos para adaptarlos comunicativamente a cada
+				tipo de red social y les damos presencia global con la traducción a diferentes idiomas como
+				el inglés, alemán o portugués.
+			</p>
+		</article>
+		<img src="/assets/images/img-3.webp" alt="" />
+		<img src="/assets/images/img-4.webp" alt="" />
+		<article>
+			<h4>Creación, gestión y difusión de contenidos digitales</h4>
+			<ul>
+				<li>Manejo de redes sociales</li>
+				<li>Creación, gestión y difusión de contenidos digitales</li>
+				<li>Desarrollo y diseño de Landing Pages y Webs</li>
+				<li>Diseño y optimización de tiendas online</li>
+				<li>Posicionamiento online de la marca</li>
+				<li>Publicidad en Facebook, Instagram, Google y Linkedin</li>
+			</ul>
+		</article>
+	</section>
+	<section>
+		<article>
+			<h3>COMUNICACIÓN EMPRESARIAL</h3>
+			<h4>Ayudamos a tu empresa a generar valor sea cual sea el nivel de madurez digital</h4>
+			<p>
+				La visión estratégica de la comunicación corporativa permite la construir la interacción,
+				dialogo y reconocimiento mutuo que incrementa y consolida la imagen y reputación de la
+				empresa u organización. Generamos mayor valor intangible al consolidar la confianza de las
+				mismas por sus públicos de interés.
+			</p>
+		</article>
+		<img src="/assets/images/img-5.webp" alt="" />
+		<img src="/assets/images/img-6.webp" alt="" />
+		<article>
+			<h4>Identidad e imagen corporativa</h4>
+			<ul>
+				<li>Desarrollo de marca</li>
+				<li>Posicionamiento de Imagen</li>
+				<li>Comunicación estratégica</li>
+				<li>Gestión de contenidos</li>
+				<li>Manejo de redes</li>
+				<li>Entrenamiento y coaching en liderazgo</li>
+			</ul>
+		</article>
+	</section>
+	<section>
+		<article>
+			<h3>COMUNICACIÓN POLÍTICA</h3>
+			<h4>Lo que no se comunica, no existe.</h4>
+			<p>
+				Hoy el éxito de cualquier campaña electoral o de política pública se sustenta en el diseño y
+				ejecución de una estrategia de comunicación planificada, con objetivos claros, medios de
+				difusión adecuados, mensajes concretos y públicos debidamente definidos.
+			</p>
+		</article>
+		<img src="/assets/images/img-7.webp" alt="" />
+		<img src="/assets/images/img-8.webp" alt="" />
+		<article>
+			<h4>Diseño y desarrollo de estrategia comunicativa</h4>
+			<ul>
+				<li>Análisis de entorno</li>
+				<li>Liderazgo</li>
+				<li>Manejo y posicionamiento de imagen</li>
+				<li>Estrategia y líneas de comunicación</li>
+				<li>Estrategia digital</li>
+				<li>Asesoramiento de prensa</li>
+				<li>Estrategia digital</li>
+			</ul>
+		</article>
+	</section>
+	<section>
+		<article>
+			<h3>TEAM</h3>
+			<h4>
+				Nos reta la creatividad, la innovación, la calidad y entrega de resultados tangibles.
+				Priorizamos el trabajo en equipo como motor del crecimiento y generación de valor.
+			</h4>
+			<p>
+				Somos un equipo interdisciplinario de programadores, músicos, politólogos, lingüistas,
+				filósofos, expertos en comunicación, branding y marketing digital, pensando para vos.
+			</p>
+		</article>
+		<img src="/assets/images/img-9.webp" alt="" />
+	</section>
+</main>
+
+<footer>
+	<div>
+		<h3>PM Comunicaciones</h3>
+		<ul>
+			<li>
+				<a href="https://wa.link/bje7mw" target="_blank" rel="noopener nofollower"
+					><WhatsappIcon />+54 9 11 4063-6734</a
+				>
+			</li>
+			<li>
+				<a href="mailto:info@pmcomunicaciones.com.ar" target="_blank" rel="noopener nofollower"
+					><EmaiIcon />info@pmcomunicaciones.com.ar</a
+				>
+			</li>
+		</ul>
+	</div>
+    <div>
+        <h3>Dejanos tu consulta</h3>
+        <form action="">
+            <div class="f-group">
+                <input type="text" required>
+                <input type="email" required>
+            </div>
+            <div class="f-group">
+                <textarea id="" cols="30" rows="10" required></textarea>
+            </div>
+            <div class="submit-wrapper">
+                <input type="submit" value="Enviar">
+            </div>
+        </form>
+    </div>
+</footer>
+
+<style>
+</style>
