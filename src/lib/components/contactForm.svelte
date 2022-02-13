eabbd9da4bdcbb4a839507131d60ed8068987d32
Uciel Sola <solauciel@gmail.com>
2022-02-12 21:14:30 -0300

    contact form moved to own component

---
+++ src/lib/components/contactForm.svelte
@@ -0,0 +1,15 @@
+<div>
+		<h3>Dejanos tu consulta</h3>
+		<form action="">
+			<div class="f-group">
+				<input type="text" required />
+				<input type="email" required />
+			</div>
+			<div class="f-group">
+				<textarea id="" cols="30" rows="10" required />
+			</div>
+			<div class="submit-wrapper">
+				<input type="submit" value="Enviar" />
+			</div>
+		</form>
+	</div>
