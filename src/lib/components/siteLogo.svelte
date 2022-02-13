80f8a2ebef462f21e94541a6792702345dcb121c
Uciel Sola <solauciel@gmail.com>
2022-02-13 15:37:33 -0300

    Site logo moved to own component

---
+++ src/lib/components/siteLogo.svelte
@@ -0,0 +1,51 @@
+<div class="logo-wrapper">
+	<img src="/assets/images/logo.webp" alt="PM Logo" />
+	<div class="text-wrapper">
+		<h1 class="title">Comunicaciones</h1>
+		<h2 class="title">La comunicación constituye y a la vez fluye como la vida.</h2>
+	</div>
+</div>
+
+<style>
+	.logo-wrapper {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		padding-inline: 15px;
+	}
+
+	img {
+		width: 15rem;
+	}
+
+	h1 {
+		font-size: clamp(2rem, 8vw, 6rem);
+		font-weight: 500;
+		text-align: center;
+		text-transform: uppercase;
+	}
+
+	h2 {
+		font-style: italic;
+		font-weight: 400;
+		font-size: 0.95rem;
+		text-align: center;
+	}
+
+	@media screen and (min-width: 1024px) {
+		.logo-wrapper {
+			flex-direction: row;
+			padding-inline: 0;
+		}
+		h1 {
+			font-weight: 400;
+			text-align: left;
+			text-transform: capitalize;
+		}
+
+		h2 {
+			text-align: left;
+			font-size: 2rem;
+		}
+	}
+</style>
