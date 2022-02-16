c29273dd08c21834ffd6d5f72867fefbdf86932b
Uciel Sola <solauciel@gmail.com>
2022-02-15 22:22:26 -0300

    latest styles changes

--- src/lib/components/contactForm.svelte
+++ src/lib/components/contactForm.svelte
@@ -31,11 +31,29 @@
 	<h3>Dejanos tu consulta</h3>
 	<form on:submit={(e) => formSubmit(e)} id="contact">
 		<div class="f-group">
-			<input id="input-name" type="text" name="name" bind:value={name} required />
-			<input type="email" name="email" bind:value={email} required />
+			<label for="name">Nombre</label>
+			<input
+				id="input-name"
+				type="text"
+				name="name"
+				bind:value={name}
+				placeholder="Nombre"
+				required
+			/>
+			<label for="email">Email</label>
+			<input type="email" name="email" bind:value={email} placeholder="Email" required />
 		</div>
 		<div class="f-group">
-			<textarea id="" cols="30" rows="10" name="message" bind:value={message} required />
+			<label for="message">Mensage</label>
+			<textarea
+				id=""
+				cols="30"
+				rows="10"
+				name="message"
+				bind:value={message}
+				placeholder="Mensaje"
+				required
+			/>
 		</div>
 		<div class="submit-wrapper">
 			<input type="submit" value="Enviar" />
@@ -43,13 +61,56 @@
 	</form>
 </div>
 
-<div class={!hideForm ? 'fade-in' : ''}>
-	<h3>Gracias {name} por tu consulta!</h3>
-</div>
-
 <style>
-	.fade-in {
+	/* .fade-in {
 		-webkit-animation: fade-in 0.5s cubic-bezier(0.39, 0.575, 0.565, 1) 1.21s both;
 		animation: fade-in 0.5s cubic-bezier(0.39, 0.575, 0.565, 1) 1.21s both;
+	} */
+
+	form,
+	.f-group {
+		display: flex;
+		flex-direction: column;
+	}
+
+	label {
+		display: none;
+	}
+
+	input,
+	textarea {
+		padding: 1rem;
+		margin-bottom: 0.5rem;
+		border: none;
+		border-radius: var(--border-radius);
+		font-family: var(--body-ff);
+	}
+
+	input[type='submit'] {
+		padding: 0.3rem 0.7rem;
+		color: #fff;
+		border: 2px solid #fff;
+		border-radius: var(--border-radius);
+		box-shadow: var(--box-shadow);
+		background-color: var(--highlight);
+	}
+
+	input[type='submit']:active {
+		box-shadow: none;
+	}
+
+	@media screen and (min-width: 1024px) {
+		.f-group {
+			flex-direction: row;
+		}
+		input,
+		textarea {
+			margin-inline: 0.5rem;
+		}
+
+		textarea {
+			width: 100%;
+		}
+		
 	}
 </style>
