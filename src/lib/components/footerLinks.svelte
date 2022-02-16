0faca0bc0abb5308399d0a788264fb8d5f40a907
Uciel Sola <solauciel@gmail.com>
2022-02-15 22:22:47 -0300

    new component for foote links

---
+++ src/lib/components/footerLinks.svelte
@@ -0,0 +1,32 @@
+<script>
+	import EmaiIcon from '$lib/components/emaiIcon.svelte';
+	import WhatsappIcon from '$lib/components/whatsappIcon.svelte';
+</script>
+
+<div class="links-container">
+	<a href="https://wa.link/bje7mw" target="_blank" rel="noopener nofollower"
+		><WhatsappIcon fill="#fff" /><span>+54 9 11 4063-6734</span></a
+	>
+
+	<a href="mailto:info@pmcomunicaciones.com.ar" target="_blank" rel="noopener nofollower"
+		><EmaiIcon fill="#fff" /><span>info@pmcomunicaciones.com.ar</span></a
+	>
+</div>
+
+<style>
+	.links-container {
+		display: flex;
+		flex-direction: column;
+	}
+
+	a {
+		display: flex;
+		margin-block: 0.5rem;
+		color: #fff;
+		text-decoration: none;
+		font-size: 1rem;
+	}
+	a span {
+		margin-left: 1rem;
+	}
+</style>
